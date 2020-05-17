0ce25543feddb69dc4a68e43eaecbc00311c5505
Nidhin D <dev.nidhin.d@gmail.com>
2020-05-17 16:21:23 +0530

    heroku config added

--- lib/colly_web/endpoint.ex
+++ lib/colly_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule CollyWeb.Endpoint do
   ]
 
   socket "/socket", CollyWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
